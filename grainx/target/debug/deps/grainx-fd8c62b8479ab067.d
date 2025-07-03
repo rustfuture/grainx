bc4c5fa1f9a243667f5115e53cb57a511cdfe4f0
rustfuture <yalikoyrecordz@gmail.com>
2025-07-03 15:03:32 +0000

    Remove additional tracked build artifacts from Git history

--- grainx/target/debug/deps/grainx-fd8c62b8479ab067.d
+++
@@ -1,8 +0,0 @@
-/home/macintosh/Desktop/rustprojects/grainx/grainx/target/debug/deps/grainx-fd8c62b8479ab067.d: src/main.rs src/rendering.rs src/analytics.rs src/network.rs
-
-/home/macintosh/Desktop/rustprojects/grainx/grainx/target/debug/deps/grainx-fd8c62b8479ab067: src/main.rs src/rendering.rs src/analytics.rs src/network.rs
-
-src/main.rs:
-src/rendering.rs:
-src/analytics.rs:
-src/network.rs:
